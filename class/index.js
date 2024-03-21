1a57750341f4b726e08c7e3eb52ff8b5fb3a8a1e
Bachan-kubi <felixghagra@gmail.com>
2024-03-21 20:31:12 +0600

    second class

--- class/index.js
+++ class/index.js
@@ -9,15 +9,20 @@ class myClass {
 }
 // 1 define class and its method under constructor
 class Users {
-    constructor(name, age, address){
+    constructor(name, age, address, introText){
         this.name = name;
         this.age = age;
         this.address = address;
+        this.introText = introText;
     }
     greet(){
         console.log(`Hi! i am ${this.name}. My age is ${this.age} and my address is at ${this.address}`);
     }
+    intro(){
+        console.log(`${this.introText}`);
+    }
 }
+// 02 create object
 const ami = {
     name: "kubi",
     age: 30,
@@ -29,5 +34,8 @@ kubi.greet();
 const putul = {
     name: "Putul Nokrek",
     age: 25,
-    address: "Nalchapra"
+    address: "Nalchapra",
+    intro: 'I am a classical dancer from my childhood. I love to dance and make other people entertain through dancing!. I get money from doing and performing in different events at locally and nationall.'
 }
+const me = new Users(putul.name, putul.age, putul.address, putul.intro);
+me.intro();
