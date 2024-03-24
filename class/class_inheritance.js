57ab5b4f19c4b0185a3c5802afd4958086b926fa
Bachan-kubi <felixghagra@gmail.com>
2024-03-24 20:52:09 +0600

    setter and getter and method identifies with new class

--- class/class_inheritance.js
+++ class/class_inheritance.js
@@ -1,1 +1,31 @@
 // Extends reserve word
+class Animal{
+    constructor(name, food){
+        this.name = name;
+        this.food = food;
+        this._speed = 0;
+    }
+    // get and set method added to learn 
+    get speed (){
+        return this._speed;
+    }
+    set speed (value){
+        if(typeof value !== 'number' || value<0){
+            console.log('not a number!');
+        }
+        return this._speed = value;
+    }
+    // below is method
+    run(){
+        console.log(`my ${this.name} can run ${this.speed} miles`);
+    }
+    eat(){
+        console.log(`my dog eats rice and pulse as his fav ${this.food}`);
+    }
+};
+
+const lili = new Animal('nobo', 'dal');
+
+lili.speed = 25;
+lili.run();
+lili.eat();
--- class/getterSettrer.js
+++ class/getterSettrer.js
@@ -2,21 +2,36 @@
 class Person {
   constructor(name) {
     this.name = name;
-    this._age = age;
+    this._age = 0;
   }
   // getter for age
-  getter() {
+  get age() {
     return this._age;
   }
   // setter for age with validation
-  setter(value) {
+  set age(value) {
     if (typeof value !== "number" || value < 0) {
       throw new Error("invalid numbers!");
     }
     this._age = value;
   }
-  // intgroduction of person
+  // introduction of person
   intro() {
     console.log(`Hello i am ${this.name} my age is ${this._age}`);
   }
 }
+// create persons instances 
+const kubi = new Person("Bachan");
+// set invalid age
+try {
+    // set age
+    kubi.age=25;
+    // kubi.age = 25
+} catch (error) {
+    console.error(error.message);
+}
+// call intro
+kubi.intro();
+
+
+  
